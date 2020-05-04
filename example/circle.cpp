c2af4f2d18b9ce946fead1bd0a307e5520a8e78b
zmx <892298182@qq.com>
2020-05-04 12:30:22 +0800

    add shape.hpp: 增加 Shape 的直线, 线段, 多边形子类

--- example/circle.cpp
+++ example/circle.cpp
@@ -1,7 +1,8 @@
 #include "castle.hpp"
+#include "shape.hpp"
 #include "time_stamp.hpp"
-#include <initializer_list>
 using namespace std;
+/*
 class CirclePixel {
 	Coordinate<Float> C;
 	Float r;
@@ -18,81 +19,7 @@ public:
 	Float radius() const { return r; }
 	Float sdf(Coordinate<Float> P) const { return abs(P - C) - r; }
 };
-
-typedef Coordinate<Float> Point;
-class Line {
-	Float a, b, c; // a x + b y + c = 0
-
-public:
-	Line(const Float &a, const Float &b, const Float &c)
-		: a(a)
-		, b(b)
-		, c(c) {}
-
-	Line(const Point &p1, const Point &p2)
-		: a(p2.y - p1.y)
-		, b(p1.x - p2.x)
-		, c(p2.x * p1.y - p1.x * p2.y) {}
-
-	Float sdf(const Point &P) const {
-		return abs(a * P.x + b * P.y + c) / hypot(a, b);
-	}
-};
-
-class Segment {
-	Point p1, p2;
-
-public:
-	Segment(const Point &p1, const Point &p2)
-		: p1(p1)
-		, p2(p2) {}
-
-	Float sdf(const Point &p) const {
-		Point edge = p1 - p2, r1 = p1 - p, r2 = p2 - p;
-		if (edge.x * r1.x + edge.y * r1.y <= 0) // p p1 p2 是钝角
-			return abs(r1);
-		if (edge.x * r2.x + edge.y * r2.y >= 0) // p p2 p1 是钝角
-			return abs(r2);
-		return abs(r1.x * edge.y - edge.x * r1.y) / abs(edge);
-	}
-};
-
-class Polygon {
-	vector<Point> vertices; // 沿边界逆时针方向
-
-public:
-	Polygon(const vector<Point> &v)
-		: vertices(v) {}
-
-	Polygon(const initializer_list<Point> &l)
-		: vertices(l) {}
-
-	Float sdf(const Point &P) const {
-		if (vertices.empty())
-			cerr << "a polygon must have at least one vertex\n";
-		if (vertices.size() == 1)
-			return abs(vertices.front() - P);
-		// 求到每一边的最短距离
-		Float dist = Segment(vertices.front(), vertices.back()).sdf(P);
-		for (size_t i = 1; i < vertices.size(); ++i) {
-			Float tmp = Segment(vertices[i - 1], vertices[i]).sdf(P);
-			if (tmp < dist)
-				dist = tmp;
-		}
-
-		vector<Point> radius(vertices.size());
-		for (size_t i = 0; i < radius.size(); ++i) {
-			radius[i] = vertices[i] - P;
-		}
-
-		Float winding_number = angle(radius.back(), radius.front());
-		for (size_t i = 1; i < vertices.size(); ++i) {
-			winding_number += angle(radius[i - 1], radius[i]);
-		}
-		winding_number /= Const::tau;
-		return int(round(winding_number)) % 2 == 0 ? dist : -dist;
-	}
-};
+*/
 
 template <typename Shape>
 void render_shape(ColorExtPicture &pic, const Shape &shape,
@@ -120,6 +47,9 @@ void render_shape(ColorExtPicture &pic, const Shape &shape, ColorExt InColor,
 int main() {
 	TimeStamp _("Render");
 	ColorExtPicture pic(1000, 1000);
+	using namespace CASTLE::Shape;
+	using namespace CASTLE;
+	// Circle shape(Point(500, 500), 200);
 	// Segment shape(Point(400, 400), Point(600, 600));
 	// Polygon shape({Point(400,400), Point(600,400), Point(600,600),
 	// Point(400,600)});
