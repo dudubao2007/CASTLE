f6dc6daa49e30f4934bf19626bccef7ec9c93991
dudubao2007 <1356523506@qq.com>
2020-05-29 18:35:01 +0800

    Format the example

--- example/Fractal.cpp
+++ example/Fractal.cpp
@@ -1,69 +1,48 @@
-#include <bits/stdc++.h>
 #include "castle.hpp"
+#include <bits/stdc++.h>
 using namespace std;
 Axis axis = Axis(1024, 1024, 2048, 2048);
-Float helper(Point a, Dword n)
-{
-	if (n == 0u)
-	{
+Float helper(Point a, Dword n) {
+	if (n == 0u) {
 		return static_cast<Float>(0.0);
 	}
 	a.x *= 3.0;
 	a.y *= 3.0;
-	if (a.x >= 1.0 && a.x <= 2.0 && a.y >= 1.0 && a.y <= 2.0)
-	{
-		return static_cast<Float>(min(min(a.x - 1.0, 2.0 - a.x), min(a.y - 1.0, 2.0 - a.y)));
+	if (a.x >= 1.0 && a.x <= 2.0 && a.y >= 1.0 && a.y <= 2.0) {
+		return static_cast<Float>(
+			min(min(a.x - 1.0, 2.0 - a.x), min(a.y - 1.0, 2.0 - a.y)));
 	}
-	while (a.x >= 1.0) a.x -= 1.0;
-	while (a.y >= 1.0) a.y -= 1.0;
+	while (a.x >= 1.0)
+		a.x -= 1.0;
+	while (a.y >= 1.0)
+		a.y -= 1.0;
 	return helper(a, n - 1u) * 0.5;
 }
-Float sdf(Point a)
-{
-	if (a.x <= 0.0 || a.x >= 1.0 || a.y <= 0.0 || a.y >= 1.0)
-	{
-		if (a.x <= 0.0)
-		{
-			if (a.y <= 0.0)
-			{
+Float sdf(Point a) {
+	if (a.x <= 0.0 || a.x >= 1.0 || a.y <= 0.0 || a.y >= 1.0) {
+		if (a.x <= 0.0) {
+			if (a.y <= 0.0) {
 				return hypot(a.x, a.y);
 			}
-			if (a.y >= 1.0)
-			{
+			if (a.y >= 1.0) {
 				return hypot(a.x, a.y - 1.0);
-			}
-			else
-			{
+			} else {
 				return -a.x;
 			}
-		}
-		else if (a.x >= 1.0)
-		{
-			if (a.y <= 0.0)
-			{
+		} else if (a.x >= 1.0) {
+			if (a.y <= 0.0) {
 				return hypot(a.x - 1.0, a.y);
-			}
-			else if (a.y >= 1.0)
-			{
+			} else if (a.y >= 1.0) {
 				return hypot(a.x - 1.0, a.y - 1.0);
-			}
-			else
-			{
+			} else {
 				return a.x - 1.0;
 			}
-		}
-		else
-		{
-			if (a.y <= 0.0)
-			{
+		} else {
+			if (a.y <= 0.0) {
 				return -a.y;
-			}
-			else if (a.y >= 1.0)
-			{
+			} else if (a.y >= 1.0) {
 				return a.y - 1.0;
-			}
-			else
-			{
+			} else {
 				assert(0);
 				return Const::NaN;
 			}
@@ -71,17 +50,15 @@ Float sdf(Point a)
 	}
 	return helper(a, 6);
 }
-Color renderer(Dword i, Dword j)
-{
-	if (sdf(axis.to_point(i, j)) <= 2.0 / axis.unit()) return Color(0, 0, 0);
+Color renderer(Dword i, Dword j) {
+	if (sdf(axis.to_point(i, j)) <= 2.0 / axis.unit())
+		return Color(0, 0, 0);
 	return Color(255, 255, 255);
 }
-int main()
-{
+int main() {
 	ColorPicture pic(4096, 4096);
 	Render::render(pic, function<Color(Dword, Dword)>(renderer), 4u);
 	BMPFile fractal(pic);
 	fractal.output("fractal_square.bmp");
 	return 0;
 }
-
