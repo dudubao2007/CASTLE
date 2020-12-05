ffed53d30adc80b6c3c2975edf13d19a213e8be7
dudubao2007 <1356523506@qq.com>
2020-12-05 18:00:55 +0800

    Fix a bug, add ColorHSV
    Fix a bug, add ColorHSV

--- color.hpp
+++ color.hpp
@@ -1,6 +1,7 @@
 #ifndef COLOR_HPP
 #define COLOR_HPP
 
+#include "const.hpp"
 #include "global_define.hpp"
 #include <array>
 #include <cassert>
@@ -64,8 +65,8 @@ public:
 	ColorExt(const Color &c)
 		: ColorExt(B2F(c.r), B2F(c.g), B2F(c.b)) {}
 
-	ColorExt(const std::array<Byte, 3> &c)
-		: ColorExt(B2F(c[0]), B2F(c[1]), B2F(c[2])) {}
+	// ColorExt(const std::array<Byte, 3> &c)
+	//	: ColorExt(B2F(c[0]), B2F(c[1]), B2F(c[2])) {}
 
 	operator Color() const { return Color(F2B(_r), F2B(_g), F2B(_b)); }
 
@@ -77,9 +78,9 @@ public:
 		return std::array<Float, 3>({_r, _g, _b});
 	}
 
-	operator std::array<Byte, 3>() const {
-		return std::array<Byte, 3>({F2B(_r), F2B(_g), F2B(_b)});
-	}
+	// operator std::array<Byte, 3>() const {
+	//	return std::array<Byte, 3>({F2B(_r), F2B(_g), F2B(_b)});
+	//}
 
 	Float r() const { return _r; }
 	Float g() const { return _g; }
@@ -119,4 +120,105 @@ public:
 
 void compose(ColorExt &a, const ColorExt &b) { a.compose(b); }
 
+class ColorHSV {
+	Float _h, _s, _v, _a;
+	static Float fit(Float x) { return x < 0 ? 0.0 : (x > 1 ? 1.0 : x); }
+
+public:
+	ColorHSV() { _h = _s = _v = _a = 0; }
+	ColorHSV(const ColorExt &);
+	ColorHSV(Float h, Float s, Float v, Float a = 0)
+		: _h(fit(h / Const::tau) * Const::tau)
+		, _s(fit(s))
+		, _v(fit(v))
+		, _a(fit(a)) {}
+
+	operator ColorExt() {
+		const Float cst = Const::pi / 3.0;
+		int hi = _h / cst;
+		Float f, p, q, t;
+		f = _h / cst - hi;
+		p = _v * (1.0 - _s);
+		q = _v * (1.0 - f * _s);
+		t = _v * (1.0 - (1.0 - f) * _s);
+		switch (hi) {
+		case 0:
+			return ColorExt(_v, t, p);
+		case 1:
+			return ColorExt(q, _v, p);
+		case 2:
+			return ColorExt(p, _v, t);
+		case 3:
+			return ColorExt(p, q, _v);
+		case 4:
+			return ColorExt(t, p, _v);
+		case 5:
+			return ColorExt(_v, p, q);
+		}
+	}
+};
+
+ColorHSV::ColorHSV(const ColorExt &c) {
+	Float r = c.r(), g = c.g(), b = c.b();
+	Float cmin, delta;
+	const Float cst = Const::pi / 3.0;
+	char vc, mc;
+	if (r > g) {
+		if (g > b)
+			vc = 'r', mc = 'b';
+		else if (r > b)
+			vc = 'r', mc = 'g';
+		else
+			vc = 'b', mc = 'g';
+	} else {
+		if (r > b)
+			vc = 'g', mc = 'b';
+		else if (g > b)
+			vc = 'g', mc = 'r';
+		else
+			vc = 'b', mc = 'r';
+	}
+	switch (vc) {
+	case 'r':
+		_v = r;
+		break;
+	case 'g':
+		_v = g;
+		break;
+	case 'b':
+		_v = b;
+		break;
+	}
+	switch (mc) {
+	case 'r':
+		cmin = r;
+		break;
+	case 'g':
+		cmin = g;
+		break;
+	case 'b':
+		cmin = b;
+		break;
+	}
+	delta = _v - cmin;
+	if (delta > 0) {
+		switch (vc) {
+		case 'r':
+			if (mc == 'b')
+				_h = cst * ((g - b) / delta);
+			else
+				_h = cst * ((g - b) / delta + 6.0);
+		case 'g':
+			_h = cst * ((b - r) / delta + 2.0);
+			break;
+		case 'b':
+			_h = cst * ((r - g) / delta + 4.0);
+			break;
+		}
+	}
+	if (_v > 0) {
+		_s = delta / _v;
+	}
+	_a = c.a();
+}
 #endif
